d4a4704f43c3f2b599793749d79027c9b8f77668
Rich Quick <rich@successfulsites.co.uk>
2011-07-29 18:39:05 +0100

    First version of the file
    First version of the file, nothing much in here yet!

---
+++ jquery.sexyslide.js
@@ -0,0 +1,30 @@
+(function($){
+ 
+    $.fn.extend({ 
+         
+        //pass the options variable to the function
+        sexySlide: function(options) {
+ 
+            //Set the default values, use comma to separate the settings, example:
+            var defaults = {
+                margin: 10,
+                thumbs : false,
+                iconsStart: true,
+				iconsFade: true
+            }
+                 
+            var options =  $.extend(defaults, options);
+ 
+            return this.each(function() {
+                var o = options;
+                var $this = $(this);
+				 
+                //code to be inserted here
+                //you can access the value like this
+                // alert(o.padding);
+             
+            });
+        }
+    });
+     
+})(jQuery);
